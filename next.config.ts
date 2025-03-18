99cec9e19619cab4a5829647b84114a6394f50de
wesleyvdk <shinxisrl@outlook.com>
2025-03-18 20:53:57 +0100

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
