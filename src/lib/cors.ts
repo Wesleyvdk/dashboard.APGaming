76a5c3ae0ae6d2ba74217cf06d799c68a9fa297c
WesleyvanderKraan0 <wesleyvanderkraan0@gmail.com>
2025-03-31 14:07:51 +0200

    updated cors

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
-const allowedOrigins = ["https://apgaming.be", "https://www.apgaming.be", "http://localhost:4000", "http://localhost:3000"]
+const allowedOrigins = ["https://ap-gaming.org", "https://www.apgaming.be", "http://localhost:4000", "http://localhost:3000"]
 
 export async function cors(response: Response | NextResponse) {
     const origin = response.headers.get("Origin") || ""
@@ -12,7 +12,7 @@ export async function cors(response: Response | NextResponse) {
         headers.set("Access-Control-Allow-Origin", origin)
     } else {
         // For security, we can set this to a specific allowed origin instead of *
-        headers.set("Access-Control-Allow-Origin", allowedOrigins[2])
+        headers.set("Access-Control-Allow-Origin", allowedOrigins[0])
     }
 
     headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
